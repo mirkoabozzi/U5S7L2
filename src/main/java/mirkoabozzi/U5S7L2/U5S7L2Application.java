d0411e57b2fe7d6cff38acbb0bfdfd15a1960157
Mirko Abozzi <mirko.abozzi@gmail.com>
2024-09-19 14:36:05 +0200

    added line on main application

--- src/main/java/mirkoabozzi/U5S7L2/U5S7L2Application.java
+++ src/main/java/mirkoabozzi/U5S7L2/U5S7L2Application.java
@@ -6,8 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class U5S7L2Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(U5S7L2Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(U5S7L2Application.class, args);
+
+        System.out.println("Commit branch develop");
+    }
 
 }
